084bd986a6df90e256fd50c3de5ad8b43ee60cd2
taisan11 <131413638+taisan11@users.noreply.github.com>
2024-08-07 18:19:50 +0900

    chore: Remove unused code and update dependencies

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { browser } from 'wxt/browser';
+import './app.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [tabs, setTabs] = useState<any[]>([]);
+
+  useEffect(() => {
+    browser.tabs.query({ currentWindow: true }).then((tabs) => {
+      setTabs(tabs);
+    });
+  }, []);
+
+  function deleteTab(tabId: number | undefined) {
+    if (tabId) {
+      browser.tabs.remove(tabId);
+    } else {
+      alert('cannot delete tab');
+    }
+    setTabs(prevTabs => prevTabs.filter(tab => tab.id !== tabId));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
+      {tabs.map((tab) => (
+        <div key={tab.id} className='aaa'>
+          <h2><a href={tab.url}>{tab.title}</a></h2>
+          <button onClick={() => deleteTab(tab.id)}>remove</button>
+          <br />
+        </div>
+      ))}
     </>
   );
 }
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  // manifestVersion:2,
+  manifest:{
+    permissions:["<all_urls>","tabs","storage"],
+    name:"tab clean",
+    description:"tab clean",
+  },
+  zip:{
+    name:"tab-clean",
+  }
 });
